d4f7a3023812c926c057781481488e3418fb6c9d
Sai-Priya-Reddy <saipriyareddy.k3@gmail.com>
2023-09-04 22:31:07 +0530

    uploading maincontent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Header from "./Components/Header/header";
 import Footer from "./Components/Footer/footer";
 import LeftSideBar from "./Components/Left_Sidebar/leftsidebar";
 import RightSideBar from "./Components/Right-Sidebar/rightsidebar";
+import Main from "./Components/Main-Content/main";
 import "./Components/Header/header.css";
 import "./Components/Footer/footer.css";
 import "./Components/Left_Sidebar/leftsidebar.css";
 import "./Components/Right-Sidebar/rightsidebar.css";
+import "./Components/Main-Content/main.css";
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Footer />
           <LeftSideBar />
           <RightSideBar/> 
+          <Main />
     </div>
   );
 }
