591cbddb65a22abc2e9a33ba278cf53abd84c38e
Sai-Priya-Reddy <saipriyareddy.k3@gmail.com>
2023-09-04 20:55:22 +0530

    uploading left sidebar

---
+++ src/Components/Left_Sidebar/leftsidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSideBar = () => {
+  return (
+    <div className="left-bar">
+      <div className="left-text">I am left sidebar</div>
+    </div>
+  );
+};
+
+export default LeftSideBar;
