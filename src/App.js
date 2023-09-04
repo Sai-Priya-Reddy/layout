be387a8f4223f969ffd74eb3b82f5ae01c35c8ef
Sai-Priya-Reddy <saipriyareddy.k3@gmail.com>
2023-09-04 10:48:14 +0530

    secondary changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./Components/Header/header";
+import "./Components/Header/header.css";
+
+
 
 function App() {
   return (
     <div className="App">
- 
+          <Header />
     </div>
   );
 }
