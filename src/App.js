7b5567e1d35354c43d6652b64ce9cbc6ab9d4061
Sai-Priya-Reddy <saipriyareddy.k3@gmail.com>
2023-09-04 11:19:14 +0530

    uploading footer file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./Components/Header/header";
+import Footer from "./Components/Footer/footer";
 import "./Components/Header/header.css";
+import "./Components/Footer/footer.css";
 
 
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
           <Header />
+          <Footer />
     </div>
   );
 }
